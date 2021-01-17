698efd080d044df2572fef69636b3964cca80f47
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-01-17 16:52:21 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const select = document.getElementById('select');
 
 var totalSeats = document.getElementById('totalSeats');
 
+populateUI();
+
+// Save selected move index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovie', movieIndex);
+    localStorage.setItem('selectedPrice', moviePrice);
+}
+
 
 // Update count & total
 function update() {
@@ -14,6 +22,9 @@ function update() {
     const seatsIndex = [...selectedSeats].map((seat) =>
         [...seats].indexOf(seat)
     );
+
+    //Local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
 
     tickets.innerText = seatsIndex.length - 1;
@@ -25,27 +36,51 @@ function update() {
     totalValue = ticketsNumber * select.value;
 
     total.innerText = `R$ ${totalValue}`;
-    totalSeats.innerText = 49 - ticketsNumber
-    
+    totalSeats.innerText = 49 - ticketsNumber;
+
 }
 
+// Populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        }); 
+    }
+
+    const selectedMovieIndex = localStorage.getItem('SelectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
 
 // Selecionar Seat
 seats.forEach(element => {
     element.addEventListener('click', (e) => {
         if(e.target.classList.contains('available')) {
-            e.target.classList.toggle('selected');    
+            e.target.classList.toggle('selected');
         } 
 
         update();
+        
     });
 });
 
 // Selecionar Filme
 select.addEventListener('change', (e) => {
 
-    update();
+    //Local storage
+    setMovieData(e.target.selectedIndex, +e.target.value);
+
+    update();   
 });
+
+update();
 
 
 
